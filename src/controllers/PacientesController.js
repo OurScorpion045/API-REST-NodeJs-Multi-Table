3538b24d3ef8f6bcc32e774b90df8d40244bc9d1
OurScorpion045 <sams24484@gmail.com>
2026-06-29 21:12:59 -0700

    Avance: Archivo PacientesController.js

---
+++ src/controllers/PacientesController.js
@@ -0,0 +1,43 @@
+import { PacientesModel } from "../models/PacientesModel.js";
+
+class PacientesController {
+
+    static async getAllPacientes(req, res) {
+        try {
+            res.writeHead(200, {
+                'Content-Type': 'application/json'
+            });
+            const results = await PacientesModel.getAllPacientes();
+            res.end(JSON.stringify(results));
+        } catch (err) {
+            res.writeHead(500);
+            res.end(JSON.stringify("Error: " + err));
+        }
+    }
+
+    static async getPacienteById(req, res, PacienteId) {
+        try {
+            res.writeHead(200, {
+                'Content-Type': 'application/json'
+            });
+            const results = await PacientesModel.getPacienteById(PacienteId);
+            res.end(JSON.stringify(results));
+        } catch (err) {
+            res.writeHead(500);
+            res.end(JSON.stringify("Error: " + err));
+        }
+    }
+
+    static async insertPaciente(req, res, DNI, Nombre, Direccion, CodigoPostal, Telefono, Genero, FechaNacimiento, Correo) {
+        try {
+            res.writeHead(200, {
+                'Content-Type': 'application/json'
+            });
+            const results = await PacientesModel.insertPaciente(DNI, Nombre, Direccion, CodigoPostal, Telefono, Genero, FechaNacimiento, Correo);
+            res.end(JSON.stringify("Paciente insertado correctamente"));
+        } catch (err) {
+            res.writeHead(500);
+            res.end(JSON.stringify("Error: " + err));
+        }
+    }
+}
