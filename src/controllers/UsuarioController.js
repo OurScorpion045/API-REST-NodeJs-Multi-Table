f9b9e1728e92c5cb608d055a9e61f41d35bad47a
OurScorpion045 <sams24484@gmail.com>
2026-06-30 13:40:59 -0700

    Avance: Archivo UsuarioController.js

---
+++ src/controllers/UsuarioController.js
@@ -0,0 +1,74 @@
+import { UsuarioModel } from "../models/UsuarioModel.js";
+import { hashPassword, hashPassword } from "../utils/bcrypt.js";
+
+class UsuarioController {
+
+    static async getAllUsuarios(req, res) {
+        try {
+            res.writeHead(200, {
+                'Content-Type': 'application/json'
+            });
+            const results = await UsuarioModel.getAllUsuarios();
+            res.end(JSON.stringify(results));
+        } catch (err) {
+            res.writeHead(500);
+            res.end(JSON.stringify("Error: " + err));
+        }
+    }
+
+    static async getUsuarioById(req, res, UsuarioId) {
+        try {
+            res.writeHead(200, {
+                'Content-Type': 'application/json'
+            });
+            const results = await UsuarioModel.getUsuarioById(UsuarioId);
+            res.end(JSON.stringify(results));
+        } catch (err) {
+            res.writeHead(500);
+            res.end(JSON.stringify("Error: " + err));
+        }
+    }
+
+    static async insertUsuario(req, res, Usuario, Password, Estado) {
+        try {
+            res.writeHead(200, {
+                'Content-Type': 'application/json'
+            });
+            const hPassword = await hashPassword(Password);
+            if (hPassword = null) {
+                throw new Error("Error al hashear la contraseña del nuevo usuario");
+            }
+            const results = await UsuarioModel.insertUsuario(Usuario, hPassword, Estado);
+            res.end(JSON.stringify("Usuario insertado correctamente"));
+        } catch (err) {
+            res.writeHead(500);
+            res.end(JSON.stringify("Error: " + err));
+        }
+    }
+
+    static async updateUsuario(req, res, Usuario, Password, Estado, UsuarioId) {
+        try {
+            res.writeHead(200, {
+                'Content-Type': 'application/json'
+            });
+            const results = await UsuarioModel.updateUsuario(Usuario, Password, Estado, UsuarioId);
+            res.end(JSON.stringify("Usuario actualizado correctamente"));
+        } catch (err) {
+            res.writeHead(500);
+            res.end(JSON.stringify("Error: " + err));
+        }
+    }
+
+    static async deleteUsuario(req, res, UsuarioId) {
+        try {
+            res.writeHead(200, {
+                'Content-Type': 'application/json'
+            });
+            const results = await UsuarioModel.deleteUsuario(UsuarioId);
+            res.end(JSON.stringify("Usuario eliminado correctamente"));
+        } catch (err) {
+            res.writeHead(500);
+            res.end(JSON.stringify("Error: " + err));
+        }
+    }
+}
