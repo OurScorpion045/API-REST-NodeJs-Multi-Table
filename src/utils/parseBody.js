7333bf1c5ed0cb0c5d07f5769f117bf5ed45b256
OurScorpion045 <sams24484@gmail.com>
2026-07-01 14:21:25 -0700

    Avance: Archivo parseBody.js

---
+++ src/utils/parseBody.js
@@ -0,0 +1,21 @@
+
+
+export const parseBody = (req) => {
+    return new Promise((resolve, reject) => {
+        let body = '';
+
+        req.on('data', chunk => {
+            body += chunk;
+        });
+
+        req.on('end', () => {
+            try {
+                resolve(JSON.parse(body));
+            } catch (err) {
+                reject(new Error("JSON invalido"));
+            }
+        })
+
+        req.on('error', reject);
+    });
+}
