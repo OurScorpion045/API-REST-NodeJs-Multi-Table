5d35b5c0763acd1f38f3e340e66bed8ae4a751d8
OurScorpion045 <sams24484@gmail.com>
2026-07-02 14:34:42 -0700

    Avance: Archivo app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+import { citasRouter } from "./src/routes/CitasRoute.js";
+
+export const processRequest = async (req, res) => {
+    let url = req.url.split('/');
+    url.shift();
+    const route = url[0];
+    
+    switch(route) {
+        case 'citas':
+            await citasRouter(req, res);
+            break;
+        default:
+            res.end(JSON.stringify("Error: ruta no valida"));
+            break;
+    }
+}
