cb6b8d833ce45f7d5ec27c86c59f279a55cac83e
OurScorpion045 <sams24484@gmail.com>
2026-07-02 14:34:00 -0700

    Avance: Archivo CitasRoute.js

--- src/routes/CitasRoute.js
+++ src/routes/CitasRoute.js
@@ -5,4 +5,43 @@ export async function citasRouter(req, res) {
     const method = req.method;
     let url = req.url.split("/");
     url.shift();
+    let id = "";
+    if (url.length > 1) {
+        id = url[1];
+    } else {
+        id = null;
+    }
+    console.log(url);
+
+    switch (method) {
+        case 'GET':
+            if (id) {
+                await CitasController.getCitaById(req, res, id);
+            } else {
+                await CitasController.getAllCitas(req, res)
+            }
+            break;
+        case 'POST':
+            let data = await parseBody(req);
+            await CitasController.insertCita(req, res, data['PacienteId'], data['Fecha'], data['HoraInicio'], data['HoraFin'], data['Estado'], data['Motivo']);
+            break;
+        case 'PUT':
+            if (id) {
+                let data = await parseBody(req);
+                await CitasController.updateCita(req, res, data['PacienteId'], data['Fecha'], data['HoraInicio'], data['HoraFin'], data['Estado'], data['Motivo'], id);
+            } else {
+                res.end(JSON.stringify("Error, id no encontrado"));
+            }
+            break;
+        case 'DELETE':
+            if (id) {
+                await CitasController.deleteCita(req, res, id);
+            } else {
+                res.end(JSON.stringify("Error, id no encontrado"));
+            }
+            break;
+        default:
+            res.end(JSON.stringify("Error, metodo no valido"));
+            break;
+    }
 }
