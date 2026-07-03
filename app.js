1ef083115d62a524af9be9c680d297c0b8a15f7c
OurScorpion045 <sams24484@gmail.com>
2026-07-03 13:03:22 -0700

    Avance: Archivo app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import { citasRouter } from "./src/routes/CitasRoute.js";
 import { pacientesRouter } from "./src/routes/PacientesRoute.js";
+import { usuarioRouter } from "./src/routes/UsuarioRoute.js";
 
 export const processRequest = async (req, res) => {
     let url = req.url.split('/');
@@ -12,6 +13,9 @@ export const processRequest = async (req, res) => {
             break;
         case 'pacientes':
             await pacientesRouter(req, res);
+            break;
+        case 'usuarios':
+            await usuarioRouter(req, res);
             break;
         default:
             res.end(JSON.stringify("Error: ruta no valida"));
